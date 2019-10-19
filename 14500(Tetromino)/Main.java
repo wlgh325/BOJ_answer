8ddfbfe5ceebd06ce1d6bd8499d57c8fc2d48765
jiho <wlgh325@naver.com>
2019-10-20 02:59:40 +0900

    init

---
+++ 14500(Tetromino)/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean[] visited;
+		String input = br.readLine();
+		N = Integer.parseInt(input);
+
+
+	}
+
+}
