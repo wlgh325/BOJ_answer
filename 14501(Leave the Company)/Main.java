e2f0ee0f994ffbd54fac25fe4b7468930cd9ffc2
jiho <wlgh325@naver.com>
2019-10-20 00:20:15 +0900

    init

---
+++ 14501(Leave the Company)/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+		String[] temp = input.split(" ");
+		boolean[] visited;
+
+		width = Integer.parseInt(temp[0]);
+		horizontalNum = Integer.parseInt(temp[1]);
+		height = Integer.parseInt(temp[2]);
+
+	}
+}
---
+++ 14501(Leave the Company)/README.md
@@ -0,0 +1,3 @@
+# BOJ 14501번 퇴사(Leave the company) 문제. java 풀이
+- 백준 온라인 저지(BOJ) 14501번 퇴사(Leave the company) 자바 풀이
+- https://www.acmicpc.net/problem/14501
