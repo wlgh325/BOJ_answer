e4966cd55ba11d7de35f94f83c8f193be7176683
Jiho <wlgh325@naver.com>
2020-01-25 01:19:22 +0900

    solve start and link problem

---
+++ 14889(스타트와_링크)/Main.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Main{	
+	static int n;
+	static int[][] arr;
+	static int rr;
+	static ArrayList<Integer> result_list;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n][n];
+		result_list = new ArrayList<>();
+		
+		boolean[] visited = new boolean[n];
+		
+		// (i,j, stat) -> i와 j와 같은 팀일때 능력치 stat만큼 가짐
+		for(int i=0; i<n; i++) {
+			String[] temp = br.readLine().split(" ");
+			for(int j=0; j<n; j++) {
+				arr[i][j] = Integer.parseInt(temp[j]);
+			}
+		}
+		
+		comb(visited, 0, n, n/2);
+		Collections.sort(result_list);
+		
+		bw.write(String.valueOf(result_list.get(0)));
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+	
+	public static void comb(boolean[] visited, int start, int n, int r) {
+		if(r==0) {
+			solve(visited);
+			return;
+		}
+		for(int i=start; i<n; i++) {
+			visited[i] = true;
+			comb(visited, i+1, n, r-1);
+			visited[i] = false;
+		}
+	}
+
+	public static void solve(boolean[] visited) {
+		int start = 0;
+		int link = 0;
+		
+		ArrayList<Integer> start_team = new ArrayList<>();
+		ArrayList<Integer> link_team = new ArrayList<>();
+		
+		// team 구분
+		for(int i=0; i<n; i++) {
+			if(visited[i])
+				start_team.add(i);
+			else
+				link_team.add(i);
+		}
+		
+		// 팀의 능력치 합 구하기
+		start = calStat(start_team);
+		link = calStat(link_team);
+		
+		result_list.add(Math.abs(start-link));
+	}
+	
+	public static int calStat(ArrayList<Integer> team) {
+		int sum = 0;
+
+		for(int i=0; i<team.size(); i++) {
+			for(int j=0; j<team.size(); j++) {
+				sum += arr[team.get(i)][team.get(j)];
+			}
+		}
+		return sum;
+	}
+}
