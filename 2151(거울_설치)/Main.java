ed837cc43790eb0f1f3072dc1b779d0c8d3e3529
Jiho <wlgh325@naver.com>
2020-02-06 22:58:00 +0900

    solve 인구이동

---
+++ 2151(거울_설치)/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+
+class Main{	
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
