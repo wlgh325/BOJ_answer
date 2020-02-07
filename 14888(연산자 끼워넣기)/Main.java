2f3a072d720e4be3ef2d9bfdb53dfb9d14f0be01
wlgh325 <wlgh325@naver.com>
2020-02-07 18:35:35 +0900

    solving;..

--- 14888(연산자 끼워넣기)/Main.java
+++ 14888(연산자 끼워넣기)/Main.java
@@ -4,12 +4,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 	static int num;
 	static int[] arr;
-	static int[] op;
+	static char[] t;
 	static ArrayList<Integer> list;
+	static char[] opList; 
+	static int idx = 0;
+	static boolean[] visited;
+	static ArrayList<Integer> resultList;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,8 +22,10 @@ public class Main {
 		
 		num = Integer.parseInt(br.readLine());
 		arr = new int[num];
-		op = new int[4];
+		t = new char[num-1];
 		list = new ArrayList<>();
+		opList = new char[num-1];
+		resultList = new ArrayList<>();
 		
 		String[] temp = br.readLine().split(" ");
 		
@@ -28,14 +35,90 @@ public class Main {
 		
 		String[] temp2 = br.readLine().split(" ");
 		for(int i=0; i<4; i++) {
-			op[i] = Integer.parseInt(temp2[i]);
+			int num = Integer.parseInt(temp2[i]);
+			initOperator(i, num);
 		}
 		
-		// 가능한 조합 구하기
-		comb();
+		visited = new boolean[num-1];
+		perm(0);
+		
+		Collections.sort(resultList);
+		System.out.println(resultList.get(resultList.size() - 1));
+		System.out.println(resultList.get(0));
 	}
 	
-	public static void comb() {
+	public static void initOperator(int k, int num) {
+		switch(k) {
+			// '+'
+			case 0:
+				for(int j=0; j<num; j++,idx++)
+					opList[idx] = '+';
+				break;
+			// '-'
+			case 1:
+				for(int j=0; j<num; j++,idx++)
+					opList[idx] = '-';
+				break;
+			// '*'
+			case 2:
+				for(int j=0; j<num; j++,idx++)
+					opList[idx] = '*';
+				break;
+			// '/'
+			case 3:
+				for(int j=0; j<num; j++,idx++)
+					opList[idx] = '/';
+		}
+	}
+	
+	public static void perm(int k)
+	{
+		if (k == num-1) {
+			resultList.add(solve());	// 계산하기
+		}
+		else
+		{
+			for (int i = 0; i < num-1; i++)
+			{
+				if (visited[i]) continue;
+
+				t[k] = opList[i];
+				visited[i] = true;
+				perm(k + 1);
+				visited[i] = false;
+			}
+		}
+	}
+	
+	public static int solve() {
+		int result = arr[0];
+		int idx = 1;
+		for(int i=0; i<t.length; i++) {
+			char op = t[i];
+			switch(op) {
+				case '+':
+					result += arr[idx];
+					break;
+				case '-':
+					result -= arr[idx];
+					break;
+				case '*':
+					result *= arr[idx];
+					break;
+				case '/':
+					result /= arr[idx];
+					if(isoverFlow())
+						
+					break;
+			}
+			idx++;
+		}
+		return result;
+	}
+	
+	public static boolean isoverFlow() {
+		boolean flag = false;
 		
+		return flag;
 	}
 }
