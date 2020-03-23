b7711ebb6e9ac5e5428de816f47140aa6c1d4d04
Jiho <wlgh325@naver.com>
2020-03-24 00:07:27 +0900

    solve 11053(LIS length, dp)

---
+++ 실버/11053(가장_긴_증가하는_부분수열)/Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<n; i++){
+			dp[i] = 1;
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for(int i=0; i<n; i++){
+			for(int j=0; j<i; j++){
+				if(arr[i] > arr[j]){
+					if(dp[i] < dp[j] + 1)
+						dp[i] = dp[j] + 1;
+				}
+			}
+		}
+		int max = 0;
+		for(int i=0; i<n; i++)
+			max = max < dp[i] ? dp[i] : max;
+		System.out.println(max);
+	}
+}
