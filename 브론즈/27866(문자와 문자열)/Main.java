c8ed0214ae4b91e7578a4e564114401eda81f574
wlgh325 <wlgh325@naver.com>
2026-03-02 01:26:11 +0900

    Solve 27866 문자와 문자열

---
+++ 브론즈/27866(문자와 문자열)/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int idx = Integer.parseInt(br.readLine());
+        System.out.println(s.charAt(idx-1));
+    }
+}
---
+++ 브론즈/27866(문자와 문자열)/README.md
@@ -0,0 +1,8 @@
+# <span style="color:orange; font-size:17pt; font-weight:bold">BOJ 27866번 문자와 문자열 자바(java) 풀이</span>
+        - 난이도: 브론즈5
+        - [백준 27866 문자와 문자열](https://www.acmicpc.net/problem/27866)
+<br><br>
+
+# <span style="color: red; font-size:15pt">문제 정리</span>
+- 문자열 S의 i 번째 문자 출력
+- i가 1부터 시작함에 유의한다.
